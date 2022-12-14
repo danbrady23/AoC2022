15bf9225e0afafa7709bfae8b060131e85afb160
Dan Brady <dan.t.brady21@gmail.com>
2022-12-14 16:56:37 +0000

    added comments to solution

--- scripts/day01.R
+++ scripts/day01.R
@@ -1,15 +1,22 @@
 # Part 1
 
-data <- read.table("data/day1.txt", sep = "\n", blank.lines.skip = F)
+# Get data in, load as dataframe but keep blank lines 
+data <- read.table("data/day01.txt", col.names = c("calories"), sep = "\n", blank.lines.skip = F)
+# Find NAs and use them to mark the different groups of number
 data["elf_no"] <- cumsum(is.na(data))
+# Remove NAs
 grouped_data <- na.omit(data)
 
-summarised_data <- aggregate(grouped_data$V1, list(grouped_data$elf_no), FUN = sum)
+# Summarise by summing by group (this would be significantly easier in tidyverse)
+summarised_data <- aggregate(grouped_data$calories, list(grouped_data$elf_no), FUN = sum)
+# Find the max value of the summarised data
 elf_with_max_cals <- max(summarised_data)
 
 # Part 2
+
+# Find top 3 by sorting the summarised data 
 sorted_summary <- sort(summarised_data$x, decreasing = T)
 
+# Then selecting the top 3 values
 n_top_elves <- 3
-
 sum(sorted_summary[1:n_top_elves])
